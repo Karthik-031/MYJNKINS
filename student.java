be1132d9e47513ae59c8678f4e9fb83b86c4dc0d
Karthik-031 <karthikram7901@gmail.com>
2025-11-17 09:53:10 +0500

    new

--- student.java
+++ student.java
@@ -1,4 +1,4 @@
-public class Student {
+public class student {
     public static void main(String[] args) {
         int rollno = 031;
         String sname = "KARTHIK";
@@ -13,6 +13,7 @@ public class Student {
 }
        
        
+
 
 
 
